4d73fb43638cca953ba1e3435dcb192904f62551
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-07-04 16:04:14 +0200

    Add: More pages

--- public/js/script.js
+++ public/js/script.js
@@ -3,39 +3,82 @@
 const elements = {
   body: document.body,
   bucket: document.getElementById('bucket_veg'),
+  m3used: document.getElementById('m3used'),
 };
+
+const config = {
+  api_url: 'https://biogas-api.herokuapp.com/api',
+  api_key: '?api_key=CMD17',
+}
 
 /* ANIMATIONS
 ----------------------------------------- */
-const timeLine = new TimelineLite();
-let drop = -140;
+const animation_2 = new TimelineLite();
+const animation_3 = new TimelineLite();
 
-timeLine.to(elements.bucket, 2.25, {
+animation_2.to(elements.bucket, 2.25, {
   rotation: 160,
   ease:Power1.easeInOut,
 })
 .staggerTo('svg .waste', 2, {
   y: -140,
   ease: Power1.easeInOut,
-  onComplete: function() {
-    drop = drop - 10;
-  }
 }, 0.25, "-=1.25")
 .to('.next', 2, {
   autoAlpha: 1,
 });
 
-function startAnimation() {
-  timeLine.play();
+animation_3.to('#Pointer', 1.5, {
+  rotation: "360_cww",
+  transformOrigin:"50% 90%",
+  ease:Linear.easeNone,
+  repeat:-1
+})
+.to('#Water', 1, {
+  fill: 'red',
+})
+
+/* MAIN APPLICATION
+----------------------------------------- */
+const app = {
+  init() {
+    animation_2.pause();
+    data.get(`${config.api_url}/status/gas${config.api_key}`, 'gas')
+  }
 }
+
+const data = {
+  get(getUrl, type) {
+    let response = '';
+    const request = new XMLHttpRequest();
+    request.open('GET', getUrl, true);
+    request.onload = () => {
+      if(request.status >= 200 && request.status < 400) {
+        response = JSON.parse(request.responseText);
+        data.insert(response, type);
+      } else {
+        console.log('error');
+      }
+    }
+    request.send()
+  },
+  insert(data, type) {
+    if(type === 'gas') {
+      let used = Math.floor(data.used);
+      let generated = Math.floor(data.generated);
+      elements.m3used.textContent = used;
+    }
+  }
+}
+
 
 /* SCROLL FUNCTIONALITY
 ----------------------------------------- */
-const smoothScroll = {
-  init(target) {
-    let actualOffset = smoothScroll.getOffset();
-    let targetPosition = document.getElementById(target).offsetTop;
-    smoothScroll.doScroll(actualOffset, targetPosition);
+const animation = {
+  init(targetElement) {
+    let actualOffset = this.getOffset();
+    let targetPosition = document.getElementById(targetElement).offsetTop;
+    this.doScroll(actualOffset, targetPosition, targetElement);
   },
   getOffset() {
     let ActualyOffset = 0;
@@ -46,16 +89,30 @@ const smoothScroll = {
     }
     return ActualyOffset;
   },
-  doScroll(actual, target) {
+  doScroll(actual, target, targetElement) {
     elements.body.classList.add('scrolling');
     elements.body.style.transform = `translate(0, -${(target - actual)}px)`;
+    this.defineNextAnimation(targetElement)
   },
-  startNextAnimation(target) {
-
+  defineNextAnimation(target) {
+    switch(target) {
+      case 'page_1':
+        console.log('1')
+        break;
+      case 'page_2':
+        this.startNextAnimation(animation_2);
+        break;
+      default:
+        console.log('no animation')
+    }
+  },
+  startNextAnimation(animation) {
+    setTimeout(function() {
+      animation.play();
+    }, 1000)
   }
 }
 
-smoothScroll.init('page_1');
-
+app.init();
 
 /* EVENT LISTENERS */
