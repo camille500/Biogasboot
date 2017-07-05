52d4eb472d15e0f3c1389b77aec6081de4dc5bc8
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-07-05 12:08:31 +0200

    Add: Recycle part

--- public/js/script.js
+++ public/js/script.js
@@ -2,8 +2,16 @@
 ----------------------------------------- */
 const elements = {
   body: document.body,
+  wastebin: document.getElementById('Wastebin'),
+  boat: document.getElementById('Boat'),
+  gas: document.getElementById('Gas'),
+  arrows: document.getElementsByClassName('recycle1'),
   bucket: document.getElementById('bucket_veg'),
-  m3: document.getElementsByClassName('m3gu')
+  waste: document.getElementsByClassName('.waste'),
+  pointer: document.getElementById('Pointer'),
+  water: document.getElementById('Water'),
+  bubble: document.getElementsByClassName('.bubble'),
+  m3: document.getElementsByClassName('m3gu'),
 };
 
 const config = {
@@ -13,15 +21,30 @@ const config = {
 
 /* ANIMATIONS
 ----------------------------------------- */
+const animation_1 = new TimelineLite();
 const animation_2 = new TimelineLite();
 const animation_3 = new TimelineLite();
-const animation_4 = new TimelineLite({repeat: -1});
+const animation_4 = new TimelineLite();
+
+animation_1.fromTo(elements.wastebin, 1, { autoAlpha: 0, },{ autoAlpha: 1, })
+.fromTo(elements.arrows[0], 1, { autoAlpha: 0, },{ autoAlpha: 1, })
+.fromTo(elements.boat, 1, { autoAlpha: 0, },{ autoAlpha: 1, })
+.fromTo(elements.arrows[1], 1, { autoAlpha: 0, },{ autoAlpha: 1, })
+.fromTo(elements.gas, 1, { autoAlpha: 0, },{ autoAlpha: 1, })
+.fromTo(elements.arrows[2], 1, { autoAlpha: 0, },{ autoAlpha: 1, })
+.fromTo(elements.wastebin, 0.5, { scale: 1, },{ scale: 1.1, })
+.to(elements.wastebin, 0.5, { scale: 1, })
+.fromTo(elements.boat, 0.5, { scale: 1, },{ scale: 1.1, })
+.to(elements.boat, 0.5, { scale: 1, })
+.fromTo(elements.gas, 0.5, { scale: 1, },{ scale: 1.1, })
+.to(elements.gas, 0.5, { scale: 1, })
+.to('.recycle', 2, { rotation: -360 });
 
 animation_2.to(elements.bucket, 2.25, {
   rotation: 160,
   ease:Power1.easeInOut,
 })
-.staggerTo('svg .waste', 2, {
+.staggerTo(elements.waste, 2, {
   y: -140,
   ease: Power1.easeInOut,
 }, 0.25, "-=1.25")
@@ -29,17 +52,17 @@ animation_2.to(elements.bucket, 2.25, {
   autoAlpha: 1,
 });
 
-animation_3.to('#Pointer', 1.5, {
+animation_3.to(elements.pointer, 1.5, {
   rotation: "360_cww",
   transformOrigin:"50% 90%",
   ease:Linear.easeNone,
   repeat:-1
 })
-.to('#Water', 10, {
+.to(elements.water, 10, {
   fill: 'green',
 });
 
-animation_4.staggerFromTo('svg .bubble', 1, {
+animation_4.staggerFromTo(elements.bubble, 1, {
   scale: 1,
   y: 0,
   ease: Power1.easeInOut,
@@ -48,12 +71,13 @@ animation_4.staggerFromTo('svg .bubble', 1, {
   scale: 1.35,
   y: -1,
   ease: Power1.easeInOut,
-}, 2)
+}, 2);
 
 /* MAIN APPLICATION
 ----------------------------------------- */
 const app = {
   init() {
+    animation_1.pause();
     animation_2.pause();
     animation_3.pause();
     data.get(`${config.api_url}/status/gas${config.api_key}`, 'gas');
@@ -111,7 +135,7 @@ const animation = {
   defineNextAnimation(target) {
     switch(target) {
       case 'page_1':
-        console.log('1')
+        this.startNextAnimation(animation_1);
         break;
       case 'page_2':
         this.startNextAnimation(animation_2);
