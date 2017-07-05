19a93f924f01910cd3c3bef5bd2b23df258da667
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-07-05 14:39:24 +0200

    Some small changes

--- public/js/script.js
+++ public/js/script.js
@@ -200,4 +200,13 @@ const animation = {
 ----------------------------------------- */
 app.init();
 
+var tooltipSpan = document.getElementById('tooltip-span');
+
+window.onmousemove = function (e) {
+    var x = e.clientX,
+        y = e.clientY;
+    tooltipSpan.style.top = (y + 20) + 'px';
+    tooltipSpan.style.left = (x + 20) + 'px';
+};
+
 /* EVENT LISTENERS */
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,10 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
+router.get('/alternative', function(req, res) {
+  res.render('alternative');
+});
+
 /* EXPORT ROUTER
 ----------------------------------------- */
 module.exports = router;
