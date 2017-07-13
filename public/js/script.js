42dba9648ec11d0afb6fd85968cb5c1b9bcca4a0
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-07-13 11:05:23 +0200

    A

--- public/js/script.js
+++ public/js/script.js
@@ -260,8 +260,8 @@
     socket.on('update', function(amount) {
       let elements = document.querySelectorAll('.amount');
       elements.forEach(function(element) {
-        let total = Number(element.textContent);
-        element.textContent = (total + amount).toFixed(2);
+        let total = Number(element.textContent).toFixed(2);
+        element.textContent = total + amount;
       })
     });
 
